c55a19bc298eb0a30a49a95d6b745c09098921d8
fikrikhoironn <fikrikf470@gmail.com>
2023-12-03 23:46:03 +0700

    feat: get statistic and history

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { createErrorResponse, createSuccessResponse } from '../utils/helper';
 import { useAuth } from '../utils/auth';
 import { profile } from '../services/userService';
+import { getStatistic } from '../services/statisticService';
+import { getHistory } from '../services/historyService';
 
 const router = Router();
 
@@ -15,5 +17,29 @@ router.get('/profile', async (req, res) => {
 		res.status(400).json(createErrorResponse(err.message));
 	}
 });
+
+router.get('/statistic', async (req, res) => {
+	try {
+		const { userId } = useAuth(req);
+		const statistic = await getStatistic(userId);
+
+		res.status(200).json(createSuccessResponse(statistic, 'Berhasil mendapatkan statistik'));
+	} catch (err) {
+		res.status(400).json(createErrorResponse(err.message));
+	}
+}
+);
+
+router.get('/history', async (req, res) => {
+	try {
+		const { userId } = useAuth(req);
+		const history = await getHistory(userId);
+
+		res.status(200).json(createSuccessResponse(history, 'Berhasil mendapatkan history'));
+	} catch (err) {
+		res.status(400).json(createErrorResponse(err.message));
+	}
+}
+);
 
 export default router;
